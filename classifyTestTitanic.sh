67cfb1f0b36e121f0d45d3c70dc5ca79c9685555
frextang <tangzt@yahoo.com>
2018-03-09 19:16:37 +0800

    Add files via upload

---
+++ classifyTestTitanic.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#rm classifyFiles.exe
+#go build src/classifyFiles.go
+classifyFiles -train=data/titanic.train.csv -test=data/titanic.test.csv -minBuck=5 -maxBuck=565 -testout=tmpout/Titanic.test.out.csv
